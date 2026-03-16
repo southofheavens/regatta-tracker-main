d3b3c8a8372a24ed07fac0651ea5c45e593afd0d
Semyon <southofheavens@mail.ru>
2026-03-17 00:35:03 +0300

    изменен init.sql. добавлен проброс сертификата в контейнер с minio

--- init.sql
+++ init.sql
@@ -3,10 +3,23 @@ CREATE TYPE user_role AS ENUM ('Participant', 'Judge');
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-	surname TEXT NOT NULL,
-	login TEXT NOT NULL,
-	password_hash TEXT NOT NULL,
-	role USER_ROLE NOT NULL,
-	tg_chat_id BIGINT
+    surname TEXT NOT NULL,
+    login TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    role user_role NOT NULL,
+    tg_chat_id BIGINT
 );
 
+CREATE TABLE races (
+    id SERIAL PRIMARY KEY,
+    start_of_the_race TIMESTAMP NOT NULL,
+    end_of_the_race TIMESTAMP NOT NULL
+);
+
+CREATE TABLE participations (
+    user_id BIGINT NOT NULL,
+    race_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, race_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (race_id) REFERENCES races(id) ON DELETE CASCADE
+);
